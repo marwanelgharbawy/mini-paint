e9e29c3c6bd3a97523867d7d56f08e22f48d2a7f
Marwan <marwanelgharbawy2@gmail.com>
2024-11-04 14:05:06 +0200

    initial commit

---
+++ src/main/java/DrawingEngine.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+
+public interface DrawingEngine {
+    // Manage shapes objects
+    public void addShape(Shape shape);
+    public void removeShape(Shape shape);
+
+    // Return the created shape objects
+    public Shape[] getShapes();
+
+    // Redraw all shapes on the canvas
+    public void refresh(Graphics canvas);
+}
---
+++ src/main/java/Shape.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.util.Map;
+
+public interface Shape {
+    // Set position
+    public void setPosition(Point position);
+    public Point getPosition();
+
+    // Update shape-specific properties, (e.g., radius)
+    public void setProperties(Map<String, Double> properties);
+    public Map<String, Double> getProperties();
+
+    // Colorize
+    public void setColor(Color color);
+    public Color getColor();
+    public void setFillColor(Color color);
+    public Color getFillColor();
+
+    // Redraw the shape on the canvas
+    public void draw(Graphics canvas);
+}
