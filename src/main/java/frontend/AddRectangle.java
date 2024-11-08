1b44bec0c55a844829cd140b375eeefe4643a8db
Marwan <marwanelgharbawy2@gmail.com>
2024-11-08 20:42:48 +0200

    add validations for rectangle

--- src/main/java/frontend/AddRectangle.java
+++ src/main/java/frontend/AddRectangle.java
@@ -1,6 +1,7 @@
 package frontend;
 
 import shapes.Rectangle;
+import backend.Validations;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,10 +11,10 @@ import java.util.Map;
 
 public class AddRectangle extends JFrame {
     private JPanel contentPane;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
+    private JTextField xField;
+    private JTextField yField;
+    private JTextField widthField;
+    private JTextField heightField;
     private JButton backButton;
     private JButton addButton;
 
@@ -26,16 +27,33 @@ public class AddRectangle extends JFrame {
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double x = Double.parseDouble(textField1.getText());
-                double y = Double.parseDouble(textField2.getText());
-                double width = Double.parseDouble(textField3.getText());
-                double height = Double.parseDouble(textField4.getText());
+
+                if (!Validations.isNumberValid(xField.getText()) || !Validations.isNumberValid(yField.getText())) {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid position.");
+                    return;
+                }
+
+                if (!Validations.isNumberValid(widthField.getText()) || !Validations.isNumberValid(heightField.getText())) {
+                    JOptionPane.showMessageDialog(null, "Please enter valid dimensions.");
+                    return;
+                }
+
+                double x = Double.parseDouble(xField.getText());
+                double y = Double.parseDouble(yField.getText());
+                double width = Double.parseDouble(widthField.getText());
+                double height = Double.parseDouble(heightField.getText());
                 Rectangle rectangle = new Rectangle();
                 rectangle.setPosition(new Point((int) x, (int) y));
                 rectangle.setProperties(Map.of("width", width, "height", height));
                 paintingPanel.addShape(rectangle);
 
                 mainWindow.updateShapeDropDown();
+                dispose();
+            }
+        });
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 dispose();
             }
         });
