97cbefcd0bf6f6e165a0fcbb6fd2a1b562997a79
Marwan <marwanelgharbawy2@gmail.com>
2024-11-08 20:49:17 +0200

    add validations for square

--- src/main/java/frontend/AddSquare.java
+++ src/main/java/frontend/AddSquare.java
@@ -1,6 +1,7 @@
 package frontend;
 
 import shapes.Square;
+import backend.Validations;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,30 +10,48 @@ import java.awt.event.ActionListener;
 import java.util.Map;
 
 public class AddSquare extends JFrame {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
+    private JPanel contentPane;
+    private JTextField xField;
+    private JTextField yField;
+    private JTextField sideField;
     private JButton backButton;
     private JButton addButton;
 
     public AddSquare(PaintingPanel paintingPanel, MainWindow mainWindow) {
-        setContentPane(panel1);
+        setContentPane(contentPane);
         setTitle("Add Square");
         setSize(320, 240);
         setVisible(true);
+
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double x = Double.parseDouble(textField1.getText());
-                double y = Double.parseDouble(textField2.getText());
-                double side = Double.parseDouble(textField3.getText());
+
+                if (!Validations.isNumberValid(xField.getText()) || !Validations.isNumberValid(yField.getText())) {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid position.");
+                    return;
+                }
+
+                if (!Validations.isNumberValid(sideField.getText())) {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid side length.");
+                    return;
+                }
+
+                double x = Double.parseDouble(xField.getText());
+                double y = Double.parseDouble(yField.getText());
+                double side = Double.parseDouble(sideField.getText());
                 Square square = new Square();
                 square.setPosition(new Point((int) x, (int) y));
                 square.setProperties(Map.of("side", side));
                 paintingPanel.addShape(square);
 
                 mainWindow.updateShapeDropDown();
+                dispose();
+            }
+        });
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 dispose();
             }
         });
