e68d0609c316be3ce275e5e7d483a72579576236
Marwan <marwanelgharbawy2@gmail.com>
2024-11-08 19:39:23 +0200

    initial colorize window

---
+++ src/main/java/frontend/Colorize.java
@@ -0,0 +1,11 @@
+package frontend;
+
+import javax.swing.*;
+
+public class Colorize {
+    private JPanel panel1;
+    private JButton redButton;
+    private JButton greenButton;
+    private JButton blueButton;
+    private JButton blackButton;
+}
